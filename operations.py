0da6547fe1cd3efdacb2dd05eae2dde4f5539807
Devashish2910 <dgpatel2910@gmail.com>
2018-03-16 14:30:51 -0500

    add: Inherited Class for Database Operations

---
+++ operations.py
@@ -0,0 +1,18 @@
+from db import DB
+
+
+class Operation(DB):
+    def __init__(self):
+        DB.__init__(self)
+
+    def insert_data(self, qry):
+        pass
+
+    def read_data(self, qry):
+        pass
+
+    def update_data(self, qry):
+        pass
+
+    def delete_data(self, qry):
+        pass
