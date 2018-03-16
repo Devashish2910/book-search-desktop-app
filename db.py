7da1f8b1cb4ad54971e9374dd5bcf93e5268d355
Devashish2910 <dgpatel2910@gmail.com>
2018-03-16 18:55:53 -0500

    Final database and display ready

--- db.py
+++ db.py
@@ -5,39 +5,57 @@ class DB:
     def __init__(self):
         conn = sqlite3.connect("book_keeping.db")
         cur = conn.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS books (name TEXT, author TEXT, price REAL, ISBN INTEGER)")
+        cur.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, author TEXT, year INTEGER, isbn TEXT)")
         conn.commit()
         conn.close()
 
     @staticmethod
-    def insert(qry):
+    def insert(name, author, year, isbn):
         conn = sqlite3.connect("book_keeping.db")
         cur = conn.cursor()
-        cur.execute(qry)
+        cur.execute("INSERT INTO books VALUES (NULL, ?, ?, ?, ?)", (name, author, year, isbn))
         conn.commit()
         conn.close()
 
     @staticmethod
-    def read(qry):
+    def view():
         conn = sqlite3.connect("book_keeping.db")
         cur = conn.cursor()
-        cur.execute(qry)
+        cur.execute("SELECT * FROM books")
         res = cur.fetchall()
         conn.close()
         return res
 
     @staticmethod
-    def update(qry):
+    def search(name="", author="", year="", isbn=""):
         conn = sqlite3.connect("book_keeping.db")
         cur = conn.cursor()
-        cur.execute(qry)
+        cur.execute("SELECT * FROM books WHERE name=? OR author=? OR year=? OR isbn=?", (name, author, year, isbn))
+        res = cur.fetchall()
+        conn.close()
+        return res
+
+    @staticmethod
+    def search_by_id(id):
+        conn = sqlite3.connect("book_keeping.db")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM books WHERE id=?", (id,))
+        res = cur.fetchall()
+        conn.close()
+        return res
+
+    @staticmethod
+    def update(_id, name, author, year, isbn):
+        conn = sqlite3.connect("book_keeping.db")
+        cur = conn.cursor()
+        cur.execute("UPDATE books SET name=?, author=?, year=?, isbn=? WHERE id=?", (name, author, year, isbn, _id))
         conn.commit()
         conn.close()
 
     @staticmethod
-    def delete(qry):
+    def delete(id):
         conn = sqlite3.connect("book_keeping.db")
         cur = conn.cursor()
-        cur.execute(qry)
+        cur.execute("DELETE FROM books WHERE id=?", (id,))
         conn.commit()
         conn.close()
--- display.py
+++ display.py
@@ -1,7 +1,11 @@
 from tkinter import *
+from db import DB
 
+op = DB()
 window = Tk()
-
+window.wm_title("Book-Search by Devashish")
+window.resizable(0,0)
+#window.wm_iconbitmap('./book.png')
 
 
 # Add labels
@@ -9,7 +13,7 @@ lbl1 = Label(window, text='Name: ')
 lbl1.grid(row=0, column=0)
 lbl2 = Label(window, text='Author: ')
 lbl2.grid(row=0, column=2)
-lbl3 = Label(window, text='Price: ')
+lbl3 = Label(window, text='Year: ')
 lbl3.grid(row=1, column=0)
 lbl4 = Label(window, text='ISBN: ')
 lbl4.grid(row=1, column=2)
@@ -17,37 +21,51 @@ lbl4.grid(row=1, column=2)
 
 
 # Add Entry
-entrybox1 = Entry(window)
+name_text = StringVar()
+entrybox1 = Entry(window, textvariable=name_text)
 entrybox1.grid(row=0, column=1)
-entrybox2 = Entry(window)
+author_text = StringVar()
+entrybox2 = Entry(window, textvariable=author_text)
 entrybox2.grid(row=0, column=3)
-entrybox3 = Entry(window)
+year_text = StringVar()
+entrybox3 = Entry(window,textvariable=year_text)
 entrybox3.grid(row=1, column=1)
-entrybox4 = Entry(window)
+ISBN_text = StringVar()
+entrybox4 = Entry(window, textvariable=ISBN_text)
 entrybox4.grid(row=1, column=3)
 
-# Add Textbox
-textbox1 = Text(window, height=15, width=40)
-textbox1.grid(row=3, column=0, columnspan=3)
-textbox1.configure(background='#4D4D4D')
+# Add Listbox
+lb = Listbox(window, height=15, width=40)
+lb.grid(row=2, column=0, rowspan=6, columnspan=2)
+lb.configure(background='#DCDCDC')
+lb.bind('<<ListboxSelect>>', get_selected_row)
+
+# Add Scrollbar
+sb = Scrollbar(window, orient=VERTICAL)
+sb.grid(row=2, column=2, rowspan=3)
+lb.configure(yscrollcommand=sb.set)
+sb.configure(command=lb.yview)
 
 # Add button
 frame1 = Frame(window)
-button1 = Button(frame1, text="Find")
-button1.grid(row=3, column=1)
+button0 = Button(frame1, text="Search a Book", command=findbook)
+button0.grid(row=2, column=3)
+button0.config(width= 15, height=2)
+button1 = Button(frame1, text="View All", command=findall)
+button1.grid(row=3, column=3)
 button1.config(width= 15, height=2)
-button2 = Button(frame1, text="Add")
-button2.grid(row=4, column=1)
+button2 = Button(frame1, text="Add a Book", command=add)
+button2.grid(row=4, column=3)
 button2.config(width= 15, height=2)
-button3 = Button(frame1, text="Delete")
-button3.grid(row=5, column=1)
+button3 = Button(frame1, text="Delete Selected", command=delete)
+button3.grid(row=5, column=3)
 button3.config(width= 15, height=2)
-button4 = Button(frame1, text="Update")
-button4.grid(row=6, column=1)
+button4 = Button(frame1, text="Update Selected", command=update)
+button4.grid(row=6, column=3)
 button4.config(width= 15, height=2)
-button5 = Button(frame1, text="Close")
-button5.grid(row=7, column=1)
+button5 = Button(frame1, text="Close", command=close)
+button5.grid(row=7, column=3)
 button5.config(width= 15, height=2)
-frame1.grid(row=3, column=3)
+frame1.grid(row=2, column=3)
 
 window.mainloop()
